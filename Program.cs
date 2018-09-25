b2ec0b6b92204dc87e579b4d6cf012f5a2a88022
Colin Kurk <colinkurk@gmail.com>
2018-09-25 16:58:19 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileIO
+
+{
+   public class Program
+    {
+      public static void Main(string[] args)
+        {
+            string userInput;
+            Console.WriteLine("Enter a number.");
+            userInput = Console.ReadLine();
+            File.WriteAllText("D:\\Documents\\FileIO\\log.txt", userInput);
+            
+            string fileRead = File.ReadAllText("D:\\Documents\\FileIO\\log.txt");
+            Console.WriteLine(fileRead);
+            Console.Read();
+        }
+
+    }
+}
